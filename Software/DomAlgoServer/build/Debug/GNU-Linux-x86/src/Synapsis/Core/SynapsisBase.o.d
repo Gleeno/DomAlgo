0676af2319c1e79c154d31e99e7077cbc18eff76
gleeno <matteo.dicarlo@gleeno.com>
2016-03-11 16:39:55 +0100

    clenaning

--- Software/DomAlgoServer/build/Debug/GNU-Linux-x86/src/Synapsis/Core/SynapsisBase.o.d
+++
@@ -1,9 +0,0 @@
-build/Debug/GNU-Linux-x86/src/Synapsis/Core/SynapsisBase.o: \
- src/Synapsis/Core/SynapsisBase.cpp src/Synapsis/Core/SynapsisBase.hpp \
- src/Synapsis/Log/Log.hpp src/Synapsis/Core/../Message/en.hpp
-
-src/Synapsis/Core/SynapsisBase.hpp:
-
-src/Synapsis/Log/Log.hpp:
-
-src/Synapsis/Core/../Message/en.hpp:
--- Software/DomAlgoServer/build/Debug/GNU-Linux-x86/src/Synapsis/Sensor/Sensor.o.d
+++
@@ -1,4 +0,0 @@
-build/Debug/GNU-Linux-x86/src/Synapsis/Sensor/Sensor.o: \
- src/Synapsis/Sensor/Sensor.cpp src/Synapsis/Sensor/Sensor.hpp
-
-src/Synapsis/Sensor/Sensor.hpp:
